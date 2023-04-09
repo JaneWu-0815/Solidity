346c0a401756eaa28c81c0727ed92332c107b9f9
吳姿眉 <jane@wuzimeideMacBook-Air.local>
2023-04-09 21:49:21 +0800

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,21 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+const INFURA_API_KEY = "ABCDE12345ABCDE12345ABCDE123456789";
+
+// Replace this private key with your Sepolia account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Beware: NEVER put real Ether into testing accounts
+const SEPOLIA_PRIVATE_KEY = "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+
+module.exports = {
+  solidity: "0.8.18",
+  networks: {
+    bscscan: {
+      url: `https://data-seed-prebsc-1-s1.binance.org:8545/`,
+      chainId: 97,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+  },
+};
